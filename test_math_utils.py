e0cf0eb4f4a07f53aafb10789087c9c4b2b496ff
N3YtoIn <153780414+N3YtoIn@users.noreply.github.com>
2025-04-16 20:41:36 -0300

    feat: error fix pt2

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,7 +1,7 @@
 import pytest
 from math_utils import add, subtract, multiply
 
-class TestMathUtils(pytest.TestReport):
+class TestMathUtils:
     def test_add(self):
         self.assertEqual(add(5, 5), 10)
         self.assertEqual(add(-1, 1), 0)
